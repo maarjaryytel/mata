03a66fc0ce4aeaafe436cffeac76dc4163f43728
Opilane TTHK <opilane@tthk.ee>
2019-09-13 18:27:06 +0300

    commit

---
+++ Operators/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //Arithmetic Operators
+        // Multiple Line Comment
+        int x = 5;
+        int y = 2;
+            /*Console.WriteLine(x + y);
+            Console.WriteLine(x - y);
+            Console.WriteLine(x * y);
+            Console.WriteLine(x / y);*/
+
+            //Console.WriteLine(x = y);
+            //Console.WriteLine(x += y);
+            //Console.WriteLine(x -= y);
+            //Console.WriteLine(x *= y);
+            //Console.WriteLine(x % y);
+            //Console.WriteLine(x /= y);
+
+            //Comparison Operators
+            /*Console.WriteLine(x == y);
+            Console.WriteLine(x != y);
+            Console.WriteLine(x > y);
+            Console.WriteLine(x >= y);
+            Console.WriteLine(x < y);
+            Console.WriteLine(x <= y);*/
+
+            
+            Console.ReadLine();
+            //Logical operators
+            //&& - AND
+            //||- OR (pipes)
+            Console.WriteLine(x != y && y == 2);
+            Console.WriteLine(x != y && y == 10);
+            Console.WriteLine(x != y || y == 2);
+            Console.WriteLine(!(y == 10));
+
+
+                
+          Console.ReadLine();
+
+        }
+    }
+}
